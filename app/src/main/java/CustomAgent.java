7ea2fb7dc20f04ff69f06b1d2f95ae74a98d0919
Michael B. Gale <github@michael-gale.co.uk>
2024-11-27 19:58:03 +0000

    Add hibernate annotations for `CustomAgent`

--- app/src/main/java/CustomAgent.java
+++ app/src/main/java/CustomAgent.java
@@ -5,11 +5,20 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
+
+@Entity
+@Table(name = "custom_agent")
 public class CustomAgent extends BaseAgent {
+  @Id
   private String name;
   private double rockProbability;
   private double paperProbability;
   private double scissorsProbability;
+  @Transient
   private double totalWeight;
 
   @JsonCreator
